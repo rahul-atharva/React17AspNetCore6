19e7a23e42120cc1f0090c1136a1afb40e0b543c
Rahul Patel <rahul.patel@atharvasystem.com>
2022-02-28 15:43:03 +0530

    add footer

--- src/frontend/react-movies/src/App.tsx
+++ src/frontend/react-movies/src/App.tsx
@@ -28,6 +28,11 @@ function App() {
           </Route> */}
         </Switch>
       </div>
+      <footer className="bd-footer py-5 mt-5 bg-light">
+            <div className="container">
+                React Movies {new Date().getFullYear().toString()}
+            </div>
+      </footer>
     </BrowserRouter>
   )
 }
